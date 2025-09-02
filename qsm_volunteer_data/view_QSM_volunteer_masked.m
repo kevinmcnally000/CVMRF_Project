a2ec903c3d1bf777f22f2a0c92fa745f9a8238dc
kevinmcnally <kevin.mcnally000@gmail.com>
2025-09-02 21:16:33 +0100

    Update view_QSM_volunteer_masked.m

--- qsm_volunteer_data/view_QSM_volunteer_masked.m
+++ qsm_volunteer_data/view_QSM_volunteer_masked.m
@@ -5,14 +5,15 @@ load('QSM_volunteer_masked.mat');
 TE_idx = 1;
 
 % View magnitude image of volunteer
-figure;
+fig(1) = figure;
 imagesc(volunteer_magnitude_mask_4d(:, :, image_idx, TE_idx));
 colormap('gray');
+cl = clim; clim = ([0, cl(2)]); % magnitude can't be -ve
 axis image off;
 title('Volunteer Magnitude');
 
 % View phase image
-figure;
+fig(2) = figure;
 imagesc(volunteer_phase_mask_4d(:, :, image_idx, TE_idx));
 colormap('gray');
 axis image off;
